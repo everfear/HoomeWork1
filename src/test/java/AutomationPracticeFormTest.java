76051a6d36ecf3f239dfc235aa87b80f25d86c54
Sergei <sergeyevrfr@gmail.com>
2024-04-21 10:03:04 +0400

    minor refactor

--- src/test/java/AutomationPracticeFormTest.java
+++ src/test/java/AutomationPracticeFormTest.java
@@ -2,7 +2,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import java.io.File;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
