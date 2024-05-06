bbfbd60cc6649e65aecc3575abeec77bce126d79
Sergei <sergeyevrfr@gmail.com>
2024-05-06 12:58:29 +0400

    preparation for pageobject tests

--- src/test/java/tests/AutomationPracticeFormTestWithPageObject.java
+++ src/test/java/tests/AutomationPracticeFormTestWithPageObject.java
@@ -4,12 +4,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
-public class AutomationPracticeFormTest
+public class AutomationPracticeFormTestWithPageObject
 {
 
     @BeforeAll
