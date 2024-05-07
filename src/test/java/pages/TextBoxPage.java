06a04dcdf1682c8fd081676fee1245fb83b38ce0
Sergei <sergeyevrfr@gmail.com>
2024-05-07 11:50:19 +0400

    added test and page for TextBoxTest

---
+++ src/test/java/pages/TextBoxPage.java
@@ -0,0 +1,75 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxPage
+{
+
+    private SelenideElement userNameInput = $("#userName"),
+            submitButton = $("#submit"),
+            emailInput = $("#userEmail"),
+            currentAddressInput = $("#currentAddress"),
+            permanentAddressInput = $("#permanentAddress");
+
+    public TextBoxPage openPage()
+    {
+
+        open("/text-box");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        return this;
+
+    }
+
+    public TextBoxPage setUserName(String value)
+    {
+
+        userNameInput.setValue(value);
+        return this;
+
+    }
+
+    public TextBoxPage setEmail(String value)
+    {
+
+        emailInput.setValue(value);
+        return this;
+
+    }
+
+    public TextBoxPage setCurrentAddress(String value)
+    {
+
+        currentAddressInput.setValue(value);
+        return this;
+
+    }
+
+    public TextBoxPage setPermanentAddress(String value)
+    {
+
+        permanentAddressInput.setValue(value);
+        return this;
+
+    }
+
+    public TextBoxPage submit()
+    {
+
+        submitButton.scrollIntoView(true);
+        submitButton.click();
+        return this;
+
+    }
+
+    public TextBoxPage checkResult(String key, String value)
+    {
+
+        $("#output").$(byText(key)).parent().shouldHave(text(value));
+        return this;
+
+    }
+}
