ab396788901055c3ec8812b7b71535506a02014f
Sergei <sergeyevrfr@gmail.com>
2024-05-11 12:17:41 +0400

    minor refactor

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -18,7 +18,7 @@ public class TestData
             userBirthDay = String.format("%02d", faker.number().numberBetween(1, 31)),
             userBirthMonth = faker.options().option("January", "February", "March", "April",
                     "May", "June", "July", "August", "September", "October", "November", "December"),
-            userBirthYear = String.format("%s",faker.number().numberBetween(1900,2200)),
+            userBirthYear = String.format("%s",faker.number().numberBetween(2000,2200)),
             subject = faker.options().option("Accounting", "Art", "Physics", "Chemistry", "Economics", "English", "Biology"),
             hobby = faker.options().option("Sports", "Reading", "Music"),
             picture = faker.options().option("cat.webp", "cat2.webp"),
