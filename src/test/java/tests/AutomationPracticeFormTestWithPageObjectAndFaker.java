ee207ddc7595057c42d230bfeff09867fedfe5ce
Sergei <sergeyevrfr@gmail.com>
2024-06-15 12:20:07 +0400

    added allure and testLogging

--- src/test/java/tests/AutomationPracticeFormTestWithPageObjectAndFaker.java
+++ src/test/java/tests/AutomationPracticeFormTestWithPageObjectAndFaker.java
@@ -1,71 +1,111 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.AutomationPracticeFormTestWithFakerPage;
+
+import static io.qameta.allure.Allure.step;
+
 
 public class AutomationPracticeFormTestWithPageObjectAndFaker extends TestBase {
 
     AutomationPracticeFormTestWithFakerPage automationPracticeFormTestPage = new AutomationPracticeFormTestWithFakerPage();
 
     @Test
+    @DisplayName("Регистрация с заполнением всех полей")
     void fillFormTestWithPageObject() {
 
-        automationPracticeFormTestPage.openPage()
+        step("Открываем страничку регистрации", () -> {
 
-                .setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setEmail(data.email)
-                .setGender(data.gender)
-                .setUserNumber(data.userNumber)
-                .setDateOfBirth(data.userBirthDay, data.userBirthMonth, data.userBirthYear)
-                .setSubjects(data.subjects)
-                .setHobbies(data.hobbies)
-                .setPicture(data.picture)
-                .setCurrentAddress(data.currentAddress)
-                .setState(data.state)
-                .setCity(data.city)
+            automationPracticeFormTestPage.openPage();
 
-                .submit()
+        });
 
-                .checkResult("Student Name", data.firstName + " " + data.lastName)
-                .checkResult("Student Email", data.email)
-                .checkResult("Gender", data.gender)
-                .checkResult("Mobile", data.userNumber)
-                .checkResult("Date of Birth", data.userBirthDay + " " + data.userBirthMonth + "," + data.userBirthYear)
-                .checkResult("Subjects", data.subjects)
-                .checkResult("Hobbies", data.hobbies)
-                .checkResult("Picture", data.picture)
-                .checkResult("Address", data.currentAddress)
-                .checkResult("State and City", data.state + " " + data.city);
+        step("Заполняем все поля и кликаем submit", () -> {
+
+            automationPracticeFormTestPage.setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setEmail(data.email)
+                    .setGender(data.gender)
+                    .setUserNumber(data.userNumber)
+                    .setDateOfBirth(data.userBirthDay, data.userBirthMonth, data.userBirthYear)
+                    .setSubjects(data.subjects)
+                    .setHobbies(data.hobbies)
+                    .setPicture(data.picture)
+                    .setCurrentAddress(data.currentAddress)
+                    .setState(data.state)
+                    .setCity(data.city)
+                    .submit();
+
+        });
+
+        step("Проверяем результат", () -> {
+
+            automationPracticeFormTestPage.checkResult("Student Name", data.firstName + " " + data.lastName)
+                    .checkResult("Student Email", data.email)
+                    .checkResult("Gender", data.gender)
+                    .checkResult("Mobile", data.userNumber)
+                    .checkResult("Date of Birth", data.userBirthDay + " " + data.userBirthMonth + "," + data.userBirthYear)
+                    .checkResult("Subjects", data.subjects)
+                    .checkResult("Hobbies", data.hobbies)
+                    .checkResult("Picture", data.picture)
+                    .checkResult("Address", data.currentAddress)
+                    .checkResult("State and City", data.state + " " + data.city);
+
+        });
 
     }
 
     @Test
+    @DisplayName("Регистрация с заполнением только обязательных полей")
     void onlyRequiredfillFormTestWithPageObject() {
 
-        automationPracticeFormTestPage.openPage()
+        step("Открываем страничку регистрации", () -> {
 
-                .setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setGender(data.gender)
-                .setUserNumber(data.userNumber)
+            automationPracticeFormTestPage.openPage();
 
-                .submit()
+        });
 
-                .checkResult("Student Name", data.firstName + " " + data.lastName)
-                .checkResult("Gender", data.gender)
-                .checkResult("Mobile", data.userNumber);
+        step("Заполняем только обязательные поля", () -> {
+
+            automationPracticeFormTestPage.setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setGender(data.gender)
+                    .setUserNumber(data.userNumber)
+                    .submit();
+
+        });
+
+        step("Проверяем результат", () -> {
+
+            automationPracticeFormTestPage.checkResult("Student Name", data.firstName + " " + data.lastName)
+                    .checkResult("Gender", data.gender)
+                    .checkResult("Mobile", data.userNumber);
+
+        });
 
     }
 
     @Test
+    @DisplayName("Неуспешная регистрация, обязательные поля пустые")
     void negativefillFormTestWithPageObject() {
 
-        automationPracticeFormTestPage.openPage()
+        step("Открываем страничку регистрации", () -> {
 
-                .submit()
+            automationPracticeFormTestPage.openPage();
 
-                .checkValidation();
+        });
 
+        step("Ничего не заполняем, сразу кликаем submit", () -> {
+
+            automationPracticeFormTestPage.submit();
+
+        });
+
+        step("Проверяем результат", () -> {
+
+            automationPracticeFormTestPage.checkValidation();
+
+        });
     }
 }
