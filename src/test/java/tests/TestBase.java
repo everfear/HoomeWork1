a689b9c72b79c0c7e3967f62af1eccff8876fbbd
Sergei <sergeyevrfr@gmail.com>
2024-06-16 15:19:08 +0400

    added getProperty

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,10 +19,12 @@ public class TestBase
     @BeforeAll
     static void beforeAll()
     {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "122.0");
+        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoidUrl", "selenoid.autotests.cloud") + "/wd/hub";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
